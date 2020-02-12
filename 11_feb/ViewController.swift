dc51b3e017c7c338146c42a4f9425e6ad316d514
annamariasimon <60967367+annamariasimon@users.noreply.github.com>
2020-02-12 11:08:15 +0000

    Initial commit

---
+++ 11_feb/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  11_feb
+//
+//  Created by Annamaria Simon on 11/02/2020.
+//  Copyright © 2020 Annamaria Simon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var shopOneLabel: UILabel!
+    @IBOutlet weak var shopTwoLabel: UILabel!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func filterButton(_ sender: Any) {
+    }
+    
+}
+

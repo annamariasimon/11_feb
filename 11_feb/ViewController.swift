1d038ff705da6aed3d2880bfa90ad715530d81b7
annamariasimon <60967367+annamariasimon@users.noreply.github.com>
2020-02-12 11:11:02 +0000

    Delete Comments

--- 11_feb/ViewController.swift
+++ 11_feb/ViewController.swift
@@ -1,15 +1,6 @@
-//
-//  ViewController.swift
-//  11_feb
-//
-//  Created by Annamaria Simon on 11/02/2020.
-//  Copyright © 2020 Annamaria Simon. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
 
     @IBOutlet weak var dateLabel: UILabel!
     

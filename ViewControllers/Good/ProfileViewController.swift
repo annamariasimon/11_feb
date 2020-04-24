b8ba2b44f6bce1df1d4f8e0c6761ea47e9cf4d5a
annamariasimon <60967367+annamariasimon@users.noreply.github.com>
2020-04-24 23:48:15 +0200

    Edits

--- ViewControllers/Good/ProfileViewController.swift
+++ ViewControllers/Good/ProfileViewController.swift
@@ -18,7 +18,6 @@ class ProfileViewController: UIViewController {
         
         setupUI()
         
-    
     }
     
     func setupUI() {
--- ViewControllers/New/extProfileViewController.swift
+++ ViewControllers/New/extProfileViewController.swift
@@ -1,17 +1,9 @@
-//
-//  extProfileViewController.swift
-//  11_feb
-//
-//  Created by Annamaria Simon on 24/04/2020.
-//  Copyright © 2020 Annamaria Simon. All rights reserved.
-//
-
 import UIKit
 
 extension ProfileViewController {
     
     func setupAvatar() {
-         profileImage.layer.cornerRadius = 10
+         profileImage.layer.cornerRadius = 5 //CHANGE
          profileImage.clipsToBounds = true
          profileImage.isUserInteractionEnabled = true
          let tapGesture = UITapGestureRecognizer(target: self, action: #selector(presentPicker))
@@ -21,7 +13,24 @@ extension ProfileViewController {
     @objc func presentPicker() {
          let picker = UIImagePickerController()
          picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        picker.delegate = self
          self.present(picker, animated: true, completion: nil)
      }
+    
+}
+
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imageSelected = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            profileImage.image = imageSelected
+        }
+        
+        if let imageOriginal = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            profileImage.image = imageOriginal
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
     
 }
